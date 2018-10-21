c75495f8c837bbab0b2848ea418c7d2e2d876512
DiannaDea <dianababurina74@gmail.com>
2018-10-21 13:51:24 +0300

    feat (config): set port for heroku

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import config from 'config';
 
 import app from './app';
 
-console.log(process.env.PORT, config);
-
-http.createServer(app.callback()).listen(process.env.PORT || config.app.port);
+http.createServer(app.callback()).listen(config.app.port);
 
 console.log('Server running on port 5000');
