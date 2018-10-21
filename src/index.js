ade78c6d837e8b6751db05adb3713e57b94e22a4
DiannaDea <dianababurina74@gmail.com>
2018-10-21 13:36:45 +0300

    feat (config): add log

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import config from 'config';
 
 import app from './app';
 
+console.log(process.env.PORT, config);
+
 http.createServer(app.callback()).listen(process.env.PORT || config.app.port);
 
 console.log('Server running on port 5000');
